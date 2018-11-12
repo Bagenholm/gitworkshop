12ec0f4539173652e1ce67cc9ddc5dd67dd2fe13
Patrik <bagenholm.patrik@gmail.com>
2018-11-12 12:21:01 +0000

    Second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
       System.out.println("Hello, Git!");
+      System.out.println("I'm successfully adding more lines to the code to commit.")
   }
 }
